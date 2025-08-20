4a1dcb8a9e970f490a71889df3c734637299cee3
Isabella751 <isabella.leite562@gmail.com>
2025-08-20 10:48:41 -0300

    API Finalizado

--- server.js
+++ server.js
@@ -44,22 +44,46 @@ app.get("/alunos/:id",(req,res) =>{
             {erro: "Aluno não encontrado."}
         )
     }
-   
 })
 
-app.post("/alunos", (req, res) => { // Fazer as validações mínimas
+app.post("/alunos", (req, res) => {
     const {nome, cpf, cep, uf, rua, numero, complemento} = req.body;
-
+    const cpfDoAluno = alunos.find( (aluno => aluno.cpf === cpf))
     if(!nome || !cpf || !cep || !uf || !rua || !numero){
         return res.status(400).json({
             erro: "Nome, cpf, uf, rua e número são obrigatórios."
         })
     }
+    else if (cpf.length !== 11){
+        return res.status(400).json({
+            erro: "CPF inválido."
+        })
+    }
+    else if(cpfDoAluno){
+        return res.status(409).json({
+            erro: "CPF duplicado."
+        })
+    }
+    else if (cep.length !== 8){
+        return res.status(400).json({
+            erro: "CEP inválido."
+        })
+    }
+    else if (uf.length !== 2){
+        return res.status(400).json({
+            erro: "UF inválido."
+        })
+    }
+    else if (!Number.isInteger(Number(numero))) {
+    return res.status(400).json({
+        erro: "Número inválido. Deve ser um número inteiro."
+    })
+    }
 
+    else{
     const id = alunos.length > 0 ? alunos[alunos.length - 1].id + 1 : 1
 
     const novoAluno = {id, nome, cpf, cep, uf, rua, numero, complemento}
-    console.log(novoAluno)
 
     alunos.push(novoAluno)
 
@@ -67,7 +91,7 @@ app.post("/alunos", (req, res) => { // Fazer as validações mínimas
         mensagem: "Aluno Cadastrado com sucesso!",
         aluno: novoAluno
     })
-   
+    }
 })
 
 app.put("/alunos/:id", (req, res)=>{
@@ -78,6 +102,22 @@ app.put("/alunos/:id", (req, res)=>{
         return res.status(400).json({
             erro: "Nome, cpf, uf, rua e número são obrigatórios."
         })
+    }
+    else if (cpf.length !== 11){
+        return res.status(400).json({
+            erro: "CPF inválido."
+        })
+        
+    }
+    else if (cep.length !== 8){
+        return res.status(400).json({
+            erro: "CEP inválido."
+        })
+    }
+    else if (uf.length !== 2){
+        return res.status(400).json({
+            erro: "UF inválido."
+        })
     }  
 
     const aluno = alunos.find(aluno => aluno.id === id)
@@ -87,7 +127,7 @@ app.put("/alunos/:id", (req, res)=>{
             erro: "Aluno não encontrado."
         })
     }
-
+    else{
     aluno.nome = nome;
     aluno.cpf = cpf;
     aluno.cep = cep;
@@ -99,10 +139,10 @@ app.put("/alunos/:id", (req, res)=>{
     res.json({
         mensagem: "Aluno Atualizado com sucesso!"
     })
-
+    }
 })
 
-app.delete("/alunos/:id", (req, res)=>{
+app.delete("/alunos/:id", (req, res)=>{ // Consertar a mensagem do deletado com sucesso
     const id = parseInt(req.params.id);
 
     const indice = alunos.findIndex(a => a.id === id)
@@ -112,14 +152,12 @@ app.delete("/alunos/:id", (req, res)=>{
             mensagem :"Aluno não encontrado."
         })
     }
-    console.log(indice)
-
+    else{
     alunos.splice(indice,1);
-
     res.status(204).json({
         mensagem: "Aluno deletado com sucesso!"
     })
-   
+    }
 })
 
 app.listen(porta, () => console.log(`Servidor rodando http://localhost:${porta}/`));""
